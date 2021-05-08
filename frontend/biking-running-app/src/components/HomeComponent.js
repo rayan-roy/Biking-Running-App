8b3f1c7d49a161a642a65291fab905aee0a27d46
d282liu <d282liu@uwaterloo.ca>
2021-05-08 16:52:43 -0400

    Updated front end

--- frontend/biking-running-app/src/components/HomeComponent.js
+++ frontend/biking-running-app/src/components/HomeComponent.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 function HomeComponent() {
+
     return (
         <div class = "home-container">
             <h1>Welcome NAME</h1>
+            <div class = "calendar">
+                <div>
+                <Calendar/>
+                </div>
+            </div>
         </div>
     )
 }
